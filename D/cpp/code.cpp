9f80e80226414456f45787bf0bc2c36fae37dfcf
r.mustafin <r.mustafin@vk.team>
2022-09-25 02:45:49 +0300

    resolve D on c++

--- D/cpp/code.cpp
+++ D/cpp/code.cpp
@@ -1,39 +1,66 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+using vi = std::vector<int>;
 
+bool canMove(int i, int j, int n, int m) {
+  return i >= 0 && j >= 0 && i < n && j < m;
+}
 
-int getLongestIncreasingPath(int n, int m, const vector<vector<int>>& matrixs) {
-    // your code goes here
+int dfs(const std::vector<vi> &matrix, int i, int j, int n, int m,
+        std::vector<vi> &distances) {
+  if (!canMove(i, j, n, m)) {
     return 0;
-}
-
-int readInt() {
-    int x;
-    cin >> x;
-    return x;
-}
-
-vector<int> readList(int n) {
-    vector<int> res(n);
-    for (int i = 0; i < n; i++) {
-        cin >> res[i];
+  }
+  if (distances[i][j] != -1) {
+    return distances[i][j];
+  }
+  int res = 1;
+  int dx[4] = {-1, 1, 0, 0};
+  int dy[4] = {0, 0, -1, 1};
+  for (int k = 0; k < 4; k++) {  // can go 4 direction
+    int newi = i + dx[k];
+    int newj = j + dy[k];
+    if (canMove(newi, newj, n, m)) {
+      if (matrix[newi][newj] > matrix[i][j] &&
+          distances[i][j] < distances[newi][newj] + 1) {
+        res = std::max(res, dfs(matrix, newi, newj, n, m, distances) + 1);
+      }
     }
-    return res;
+  }
+  return distances[i][j] = res;
 }
 
-vector<vector<int>> readMatrix(int n, int m) {
-    vector<vector<int>> matrix(n, vector<int>(m));
-    for (int i = 0; i < n; i++) {
-        matrix[i] = readList(m);
+int getLongestIncreasingPath(const std::vector<vi> &matrix) {
+  int n = matrix.size();
+  int m = matrix[0].size();
+  std::vector<vi> distances(n, vi(m, -1));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      dfs(matrix, i, j, n, m, distances);
     }
-    return matrix;
+  }
+  int ans = 1;
+
+  for (auto &row : distances) {
+    for (auto &el : row) {
+      ans = std::max(ans, el);
+    }
+  }
+
+  return ans;
 }
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> matrix = readMatrix(n, m);
-    cout << getLongestIncreasingPath(n, m, matrix);
+  // freopen("input.txt", "r", stdin);
+  int n, m;
+  scanf("%d %d", &n, &m);
+  std::vector<vi> matrix(n, vi(m));
+  for (auto &row : matrix) {
+    for (auto &el : row) {
+      scanf("%d", &el);
+    }
+  }
+  printf("%d\n", getLongestIncreasingPath(matrix));
+  return 0;
 }
