3ea64d5729fb73b8133fbe6bc20e520f3e366229
Bazys <bazyssoft@gmail.com>
2022-11-01 01:09:11 +0300

    add js and py

--- A/js/code.js
+++ A/js/code.js
@@ -1,7 +1,7 @@
 const _readline = require('readline');
 
 const _reader = _readline.createInterface({
-    input: process.stdin
+  input: process.stdin
 });
 
 const _inputLines = [];
@@ -10,7 +10,7 @@ let _curLine = 0;
 // Установим callback на считывание строки - так мы получим
 // все строки из ввода в массиве _inputLines.
 _reader.on('line', line => {
-    _inputLines.push(line);
+  _inputLines.push(line);
 });
 
 process.stdin.on('end', solve);
@@ -19,16 +19,16 @@ process.stdin.on('end', solve);
 // Функция парсит число из очередной строки массива _inputLines
 // и сдвигает указатель на единицу вперёд.
 function readNumber() {
-    return Number(_inputLines[_curLine++]);
+  return Number(_inputLines[_curLine++]);
 }
 
 function getSum(a, b) {
-    // Ваше решение
+  return a + b;
 }
 
 function solve() {
-    const a = readNumber();
-    const b = readNumber();
+  const a = readNumber();
+  const b = readNumber();
 
-    console.log(getSum(a, b));
+  console.log(getSum(a, b));
 }
--- B/js/code.js
+++ B/js/code.js
@@ -1,41 +1,47 @@
 function getCardCount(n, k, cards) {
-    // your code goes here
-    return 0;
+  var sum = cards.reduce((partialSum, a) => partialSum + a, 0);
+  var max = sum;
+  for (var i = k - 1; i >= 0; i--) {
+    sum -= cards[i];
+    sum += cards[n - k + i];
+    max = Math.max(max, sum);
+  }
+  return max;
 }
 
 
 const _readline = require('readline');
 
 const _reader = _readline.createInterface({
-    input: process.stdin
+  input: process.stdin
 });
 
 const _inputLines = [];
 let _curLine = 0;
 
 _reader.on('line', line => {
-    _inputLines.push(line);
+  _inputLines.push(line);
 });
 
 process.stdin.on('end', solve);
 
 
 function solve() {
-    const n = readInt();
-    const k = readInt();
-    const cards = readArray();
-    const ans = getCardCount(n, k, cards);
-    console.log(ans);    
+  const n = readInt();
+  const k = readInt();
+  const cards = readArray();
+  const ans = getCardCount(n, k, cards);
+  console.log(ans);
 }
 
 function readInt() {
-    const n = Number(_inputLines[_curLine]);
-    _curLine++;
-    return n;
+  const n = Number(_inputLines[_curLine]);
+  _curLine++;
+  return n;
 }
 
 function readArray() {
-    var arr = _inputLines[_curLine].trim(" ").split(" ").map(num => Number(num));
-    _curLine++;
-    return arr;
+  var arr = _inputLines[_curLine].trim(" ").split(" ").map(num => Number(num));
+  _curLine++;
+  return arr;
 }
